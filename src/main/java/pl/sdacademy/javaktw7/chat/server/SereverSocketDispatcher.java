6de3527944bc3583be7ab4435618808f0ed7c561
Adam Jakubowski <adamjski@gmail.com>
2018-09-29 14:51:53 +0200

    SereverSocketDispatcher added

---
+++ src/main/java/pl/sdacademy/javaktw7/chat/server/SereverSocketDispatcher.java
@@ -0,0 +1,33 @@
+package pl.sdacademy.javaktw7.chat.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SereverSocketDispatcher implements Runnable{
+
+
+    private final ChatLog chatLog;
+    private final ServerSocket serverSocket;
+
+    public SereverSocketDispatcher() throws IOException {
+
+        serverSocket = new ServerSocket(5567);
+        chatLog = new ChatLog();
+    }
+
+    @Override
+    public void run() {
+        while (true){
+            try{
+                Socket newClient = serverSocket.accept();
+                Runnable readerRunnable = new ServerSocketReaderRunnable(newClient, chatLog);
+                System.out.println("!!! New client connected");
+                new Thread(readerRunnable).start();
+            }catch (IOException e){
+                System.out.println("!!! Could not connect new client" + e.getMessage());
+            }
+
+       }
+    }
+}
